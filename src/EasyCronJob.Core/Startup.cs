f7f2842d661a68b6f35049033f7d04b231635df6
furkandeveloper <furkan.dvlp@gmail.com>
2021-06-08 23:26:16 +0300

    ⛏ Add Service Collection Extensions

---
+++ src/EasyCronJob.Core/Startup.cs
@@ -0,0 +1,38 @@
+﻿using EasyCronJob.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCronJob.Core
+{
+    /// <summary>
+    /// Apply Resulation
+    /// </summary>
+    /// <typeparam name="T">
+    /// Cron Job Service in CronJobService type
+    /// </typeparam>
+    /// <param name="services">
+    /// Microsoft.Extensions.DependencyInjection.IServiceCollection
+    /// </param>
+    /// <param name="action">
+    /// Cron Configuration Action Object
+    /// </param>
+    /// <returns>
+    /// Microsoft.Extensions.DependencyInjection.IServiceCollection
+    /// </returns>
+    public static class Startup
+    {
+        public static IServiceCollection ApplyResulation<T>(this IServiceCollection services, Action<ICronConfiguration<T>> action)
+            where T : CronJobService
+        {
+            var options = new CronConfiguration<T>();
+            action.Invoke(options);
+            services.AddSingleton<ICronConfiguration<T>>(options);
+            services.AddHostedService<T>();
+            return services;
+        }
+    }
+}
