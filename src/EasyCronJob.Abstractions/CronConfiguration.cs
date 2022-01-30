25f8f8a8a69c2eeab154e61de508c5ece793b142
Furkan Güngör <furkan.dvlp@gmail.com>
2022-01-30 16:05:52 +0300

    🔨 Move Cron Configuration from EasyCronJob.Core to EasyCronJob.Abstraction

---
+++ src/EasyCronJob.Abstractions/CronConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EasyCronJob.Abstractions
+{
+    /// <summary>
+    /// Cron Configuration Object
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class CronConfiguration<T> : ICronConfiguration<T>
+    {
+        /// <summary>
+        /// Cron Expression. For Example : * * * * *
+        /// </summary>
+        public string CronExpression { get; set; }
+
+        /// <summary>
+        /// Represents any time zone in the world.
+        /// </summary>
+        public TimeZoneInfo TimeZoneInfo { get; set; }
+    }
+}
