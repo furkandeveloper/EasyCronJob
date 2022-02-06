16db13417b0fbbd3315ad23534b0b52644042ea7
Furkan Güngör <furkan.dvlp@gmail.com>
2022-02-06 14:44:17 +0300

    ♻️ Re-Active job initializer

--- samples/EasyCron.Sample/Startup.cs
+++ samples/EasyCron.Sample/Startup.cs
@@ -36,22 +36,22 @@ namespace EasyCron.Sample
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "EasyCron.Sample", Version = "v1" });
             });
 
-            //services.ApplyResulation<ConsoleCronJob>(options =>
-            //{
-            //    options.CronExpression = "*/10 * * * * *";
-            //    options.TimeZoneInfo = TimeZoneInfo.Local;
-            //    options.CronFormat = Cronos.CronFormat.IncludeSeconds;
-            //});
-            //services.ApplyResulation<MyJob>(options =>
-            //{
-            //    options.CronExpression = "* * * * *";
-            //    options.TimeZoneInfo = TimeZoneInfo.Local;
-            //    options.CronFormat = Cronos.CronFormat.Standard;
-            //});
+            services.ApplyResulation<ConsoleCronJob>(options =>
+            {
+                options.CronExpression = "*/10 * * * * *";
+                options.TimeZoneInfo = TimeZoneInfo.Local;
+                options.CronFormat = Cronos.CronFormat.IncludeSeconds;
+            });
+            services.ApplyResulation<MyJob>(options =>
+            {
+                options.CronExpression = "* * * * *";
+                options.TimeZoneInfo = TimeZoneInfo.Local;
+                options.CronFormat = Cronos.CronFormat.Standard;
+            });
 
 
-            services.InitializeCronServices();
-            services.AutoConfigurer();
+            //services.InitializeCronServices();
+            //services.AutoConfigurer();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
