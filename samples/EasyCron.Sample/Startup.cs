0b92b84f57f9a2ee09cbcc43a7d012fc0802f9f5
furkandeveloper <furkan.dvlp@gmail.com>
2024-11-03 00:25:35 +0300

    ♻️ Remove unused usings from files

--- samples/EasyCron.Sample/Startup.cs
+++ samples/EasyCron.Sample/Startup.cs
@@ -1,19 +1,12 @@
 using EasyCron.Sample.Jobs;
-using EasyCronJob.AutoConfigurer;
 using EasyCronJob.Core;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace EasyCron.Sample
 {
