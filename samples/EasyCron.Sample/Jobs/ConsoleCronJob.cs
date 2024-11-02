5c6943d4201b8d41d6be9308f23638ff26a836f6
furkandeveloper <furkan.dvlp@gmail.com>
2024-11-03 00:24:31 +0300

    ♻️ Remove unused usings from ConsoleCronJob.cs

--- samples/EasyCron.Sample/Jobs/ConsoleCronJob.cs
+++ samples/EasyCron.Sample/Jobs/ConsoleCronJob.cs
@@ -1,8 +1,6 @@
 ﻿using EasyCronJob.Abstractions;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
